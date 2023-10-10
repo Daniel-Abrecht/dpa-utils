25f1fde2f810a00b7d1d7044cb737966fd0b597f
Daniel Abrecht <public@danielabrecht.ch>
2023-10-10 16:44:11 +0000

    WIP

--- src/bo-unique.c
+++ src/bo-unique.c
@@ -26,12 +26,12 @@ struct bucket {
 // Each bucket will have twice the size of the previous one.
 #define BUCKET_BASE CHAR_BIT
 #define BUCKET_LIST_COUNT (int)((sizeof(size_t)-1) * CHAR_BIT - BUCKET_BASE)
-#define BUCKET_COUNT (((size_t)1)<<(BUCKET_LIST_COUNT+BUCKET_BASE-1))
-static_assert(BUCKET_LIST_COUNT > 0, "size_t was smaller than expected");
+#define BUCKET_COUNT (((size_t)1)<<(BUCKET_LIST_COUNT+BUCKET_BASE))
 
-// Locks are really big. Like, 40 bytes big on m PC!
+// Locks are really big. Like, 40 bytes big on my PC!
 enum { LOCK_COUNT = 1<<7 };
 
+static_assert(BUCKET_LIST_COUNT > 0, "size_t was smaller than expected");
 static_assert(LOCK_COUNT <= (((size_t)1)<<BUCKET_BASE), "There can't be more locks than buckets in the first bucket list");
 static_assert(!(LOCK_COUNT & (LOCK_COUNT-1)), "LOCK_COUNT must be a power of 2");
 
@@ -59,25 +59,16 @@ static inline void init(void){
 }
 
 static inline struct bucket* get_bucket(dpa_u_hash_t hash){
-  size_t shift_size = atomic_load(&hash_map.shift_size);
-  hash = hash & ((((size_t)1)<<shift_size)-1);
+  const size_t shift_size = atomic_load(&hash_map.shift_size);
+  const size_t size_mask = (((size_t)1)<<shift_size)-1;
+  hash = hash & size_mask;
   if(hash >= atomic_load(&hash_map.move_pending))
-    hash -= ((size_t)1)<<(shift_size-1);
+    hash &= size_mask>>1;
   int i = dpa_u_log2(hash);
   if(hash < (((size_t)1)<<BUCKET_BASE))
     return &atomic_load(&hash_map.bucket[0])[hash];
-  return &atomic_load(&hash_map.bucket[i-(BUCKET_BASE-1)])[((size_t)1)<<i];
+  return &atomic_load(&hash_map.bucket[i-(BUCKET_BASE-1)])[hash&((((size_t)1)<<i)-1)];
 }
-
-/*static inline size_t entry_get_offset(const dpa__u_bo_unique_entry_t*const e){
-  size_t type_offset = e->type_offset;
-  switch(type_offset){
-    case DPA__U_BO_UNIQUE__ENTRY_TYPE_INLINE: return 0;
-    case DPA__U_BO_UNIQUE__ENTRY_TYPE_STATIC: return 0;
-    case DPA__U_BO_UNIQUE__ENTRY_TYPE_REFCOUNTED: return ((struct dpa__u_bo_entry_refcounted*)e)->offset;
-  }
-  return type_offset - 2;
-}*/
 
 static inline struct dpa_u_refcount_freeable* entry_get_ext_refcount(const dpa__u_bo_unique_entry_t*const e){
   switch(e->type_offset){
@@ -96,17 +87,17 @@ static inline void unlock_entry(dpa_u_hash_t hash){
   mtx_unlock(&lock_table[hash&(LOCK_COUNT-1)]);
 }
 
-void dpa__u_bo_unique_ref(const dpa__u_bo_unique_entry_t* _bo){
+void dpa__u_bo_unique_ref(dpa_u_bo_unique_t _bo){
   dpa__u_bo_unique_entry_t* bo = (dpa__u_bo_unique_entry_t*)_bo;
   dpa_u_refcount_ref(&bo->refcount);
 }
 
-bool dpa__u_bo_unique_put(const dpa__u_bo_unique_entry_t* _bo){
+bool dpa__u_bo_unique_put(dpa_u_bo_unique_t _bo){
   dpa__u_bo_unique_entry_t* bo = (dpa__u_bo_unique_entry_t*)_bo;
   return dpa_u_refcount_put(&bo->refcount);
 }
 
-static inline void grow(void){
+static void grow(void){
   {
     const int result = mtx_trylock(&resize_lock);
     if(result == thrd_busy)
@@ -125,16 +116,16 @@ static inline void grow(void){
   }
   atomic_store(&hash_map.bucket[shift_size-(BUCKET_BASE-1)], new_bucket);
   size_t i = 0;
-  for(int bi = 0; bi < shift_size; bi++){
+  for(int bi = 0; bi < shift_size-(BUCKET_BASE-1); bi++){
     struct bucket*const buckets = hash_map.bucket[bi];
     const size_t count = ((size_t)1) << (bi+BUCKET_BASE);
-    for(; i < count; i++){
+    for(int j=0; i < count; i++,j++){
       lock_entry(i);
-      for(dpa__u_bo_unique_entry_t*restrict e, **it=&buckets[i-count/2].next; (e=*it); it=&e->next){
+      for(dpa__u_bo_unique_entry_t*restrict e, **it=&buckets[j].next; (e=*it); it=&e->next){
         if(!(e->hash & ((size_t)1)<<shift_size))
           continue;
-        *it = 0;
         new_bucket->next = e;
+        *it = 0;
         break;
       }
       atomic_store(&hash_map.move_pending, i+1);
@@ -143,6 +134,65 @@ static inline void grow(void){
   }
 out:
   mtx_unlock(&resize_lock);
+}
+
+static void shrink(void){
+  {
+    const int result = mtx_trylock(&resize_lock);
+    if(result == thrd_busy)
+      return;
+    assert(result == thrd_success);
+  }
+  const int shift_size = atomic_load(&hash_map.shift_size) - 1;
+  if(shift_size <= BUCKET_BASE){
+    mtx_unlock(&resize_lock);
+    return;
+  }
+  struct bucket*const old_bucket = hash_map.bucket[shift_size-(BUCKET_BASE-1)];
+  const size_t old_size = ((size_t)1)<<shift_size;
+  size_t i = old_size;
+  for(int bi = shift_size-(BUCKET_BASE-1); bi--; ){
+    struct bucket*const buckets = hash_map.bucket[bi];
+    const size_t start = bi ? ((size_t)1) << (bi+BUCKET_BASE-1) : 0;
+    for(size_t j=i-start; i--,j--; ){
+      lock_entry(i);
+      atomic_store(&hash_map.move_pending, i + old_size);
+      dpa__u_bo_unique_entry_t** it=&buckets[j].next;
+      while(*it) it=&(*it)->next;
+      *it = old_bucket[i+old_size].next;
+      unlock_entry(i);
+    }
+  }
+  atomic_store(&hash_map.shift_size, shift_size);
+  atomic_store(&hash_map.bucket[shift_size-(BUCKET_BASE-1)], 0);
+  mtx_unlock(&resize_lock);
+  free(old_bucket);
+}
+
+void dpa__u_bo_unique_destroy(dpa_u_bo_unique_t bo){
+  const dpa_u_hash_t hash = bo->hash;
+  lock_entry(hash);
+  if(!dpa_u_refcount_is_zero(&bo->refcount.refcount)){
+    // Someone else picked up the entry before we got to remove it.
+    unlock_entry(hash);
+    return;
+  }
+  for(dpa__u_bo_unique_entry_t **it=&get_bucket(hash)->next, *e; (e=*it); it=&e->next){
+    if(e != bo)
+      continue;
+    *it = e->next;
+    unlock_entry(hash);
+    struct dpa_u_refcount_freeable* rc = entry_get_ext_refcount(e);
+    free(e);
+    dpa_u_refcount_put(rc);
+    const size_t count = atomic_fetch_sub(&hash_map.count, 1) - 1;
+    const size_t total_buckets = ((size_t)1) << atomic_load(&hash_map.shift_size);
+    if( total_buckets > (((size_t)1) << BUCKET_BASE) && (float)count / total_buckets <= LOAD_FACTOR_SHRINK )
+      shrink();
+    return;
+  }
+  // TODO: Print error message
+  abort();
 }
 
 dpa_u_bo_unique_t dpa__u_bo_do_intern(const dpa_u_bo_simple_ro_t bo){
@@ -241,8 +291,9 @@ dpa_u_bo_unique_t dpa__u_bo_do_intern(const dpa_u_bo_simple_ro_t bo){
     new_entry = &new->entry;
   }
   *it = new_entry;
-  const size_t count = atomic_fetch_add(&hash_map.count, 1) + 1;
   unlock_entry(hash);
+  // Note: this isn't an exact science
+  const size_t count = atomic_fetch_add(&hash_map.count, 1) + 1;
   const size_t total_buckets = ((size_t)1) << atomic_load(&hash_map.shift_size);
   if( total_buckets < BUCKET_COUNT && (float)count / total_buckets >= LOAD_FACTOR_EXPAND )
     grow();
