c1b74274a4307d15635b89e1ec47af662dde63a5
Daniel Abrecht <public@danielabrecht.ch>
2023-11-14 18:29:37 +0000

    More documentation

--- include/dpa/utils/bo.h
+++ include/dpa/utils/bo.h
@@ -22,6 +22,7 @@ enum { DPA_U_BO_INLINE_MAX_SIZE = DPA__U_BO_COMMON_SIZE-1 };
 #define DPA_U_BO_MAX_SIZE (((size_t)1)<<((sizeof(size_t)-1)*CHAR_BIT))
 #define DPA__U_BO_ALIGN alignas(struct{size_t a; void* b;})
 
+static_assert(DPA_U_BO_INLINE_MAX_SIZE <= (1<<4)-1, "It's too big to fit into a dpa_u_inline_bo_t. Please file a bug, and which platform has such huge pointers?");
 
 //////////////////////////////////
 //////      Data types      //////
