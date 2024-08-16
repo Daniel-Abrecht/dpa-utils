0e3401a711be9c4d7a251a8c430c67bf10d0189b
Daniel Abrecht <public@danielabrecht.ch>
2024-08-16 13:52:07 +0000

    WIP: Implemented all v_bo_refcounted conversions

--- include/dpa/utils/_bo/conv/bo_refcounted/v_bo_refcounted.h
+++ include/dpa/utils/_bo/conv/bo_refcounted/v_bo_refcounted.h
@@ -4,9 +4,9 @@
     dpa_u_bo_with_refcount_t*: dpa__u_bo_with_refcount__v_bo_refcounted(DPA__G(dpa_u_bo_with_refcount_t*,(X))) \
   )
 
-#define dpa__u_v_bo_refcounted__v_bo_refcounted(X)  (X).ro
-#define dpa__u_p_bo_refcounted__v_bo_refcounted(X)  (X)->ro
-#define dpa__u_cp_bo_refcounted__v_bo_refcounted(X) (X)->ro
-#define dpa__u_any_bo_refcounted__v_bo_refcounted(X) (const dpa_u_bo_refcounted_t){ .bo_simple = { .type=DPA_U_BO_REFCOUNTED, .size=dpa__u_any_bo_refcounted__get_size((X)), .data=dpa__u_any_bo_refcounted__data((X)) }, .refcount=dpa__u_any_bo_refcounted__get_refcount((X)) }
+#define dpa__u_v_bo_refcounted__v_bo_refcounted(X)  (X)
+#define dpa__u_p_bo_refcounted__v_bo_refcounted(X)  *(X)
+#define dpa__u_cp_bo_refcounted__v_bo_refcounted(X) *(X)
+#define dpa__u_any_bo_refcounted__v_bo_refcounted(X) *(dpa_u_bo_refcounted_t*)(X)
 
-#define dpa__u_bo_with_refcount__v_bo_refcounted(X) (const dpa_u_bo_refcounted_t){ .bo_simple = { .type=DPA_U_BO_REFCOUNTED, .size=dpa__u_bo_with_refcount__get_size((X)), .data=dpa__u_bo_with_refcount__data((X)) }, .refcount=dpa__u_bo_with_refcount__get_refcount((X)) }
+#define dpa__u_bo_with_refcount__v_bo_refcounted(X) *(dpa_u_bo_refcounted_t*)(X)
