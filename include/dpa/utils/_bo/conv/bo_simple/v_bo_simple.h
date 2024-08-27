422fe4f8efa7bdc81069e5203a580ac4b1f700e8
Daniel Abrecht <public@danielabrecht.ch>
2024-08-27 21:09:58 +0000

    wip

--- include/dpa/utils/_bo/conv/bo_simple/v_bo_simple.h
+++ include/dpa/utils/_bo/conv/bo_simple/v_bo_simple.h
@@ -1,25 +1,32 @@
 
 #define dpa_u_v_bo_simple(...) dpa_u_assert_selection(dpa_u_v_bo_simple_g((__VA_ARGS__)))
 #define dpa_u_v_bo_simple_g(X) dpa_u_generic((X), \
+    dpa__u_helper_v_g(bo_inline, v_bo_simple, (X)), \
     dpa__u_helper_v_g(bo_simple, v_bo_simple, (X)), \
     dpa__u_helper_v_g(bo_hashed, v_bo_simple, (X)), \
     dpa__u_helper_v_g(bo_refcounted, v_bo_simple, (X)), \
     dpa_u_bo_with_refcount_t*: dpa__u_bo_with_refcount__v_bo_simple(DPA__G(dpa_u_bo_with_refcount_t*,(X))) \
   )
 
+// TODO: check not const dpa_u_bo_inline_t
+#define dpa__u_v_bo_inline__v_bo_simple(X) dpa_u_rescope(const dpa_u_bo_simple_t, dpa__u_p_bo_inline__t_bo_simple_p((dpa_u_bo_inline_t*)(X).all.all))
+#define dpa__u_p_bo_inline__v_bo_simple(X) dpa_u_rescope(const dpa_u_bo_simple_t, dpa__u_p_bo_inline__t_bo_simple_p((X)))
+#define dpa__u_cp_bo_inline__v_bo_simple(X) dpa_u_rescope(const dpa_u_bo_simple_t, dpa__u_p_bo_inline__t_bo_simple_p((X)))
+#define dpa__u_any_bo_inline__v_bo_simple(X) dpa_u_rescope(const dpa_u_bo_simple_t, dpa__u_p_bo_inline__t_bo_simple_p((dpa_u_bo_inline_t*)(X)))
+
 #define dpa__u_v_bo_simple__v_bo_simple(X)   (X)
 #define dpa__u_p_bo_simple__v_bo_simple(X)  *(X)
 #define dpa__u_cp_bo_simple__v_bo_simple(X) *(X)
-#define dpa__u_any_bo_simple__v_bo_simple(X) dpa_u_rescope(const dpa_u_bo_simple_t, *(const dpa_u_bo_simple_t*)(X))
+#define dpa__u_any_bo_simple__v_bo_simple(X) dpa_u_rescope(const dpa_u_bo_simple_t, dpa__u_simple_fix_type(*(const dpa_u_bo_simple_t*)(X)))
 
 #define dpa__u_v_bo_hashed__v_bo_simple(X) dpa_u_rescope(const dpa_u_bo_simple_t, (X).bo_simple)
 #define dpa__u_p_bo_hashed__v_bo_simple(X) dpa_u_rescope(const dpa_u_bo_simple_t, (X)->bo_simple)
 #define dpa__u_cp_bo_hashed__v_bo_simple(X) dpa_u_rescope(const dpa_u_bo_simple_t, (X)->bo_simple)
-#define dpa__u_any_bo_hashed__v_bo_simple(X) dpa_u_rescope(const dpa_u_bo_simple_t, *(const dpa_u_bo_simple_t*)(X))
+#define dpa__u_any_bo_hashed__v_bo_simple(X) dpa_u_rescope(const dpa_u_bo_simple_t, dpa__u_simple_fix_type(*(const dpa_u_bo_simple_t*)(X)))
 
 #define dpa__u_v_bo_refcounted__v_bo_simple(X) dpa_u_rescope(const dpa_u_bo_simple_t, (X).bo_simple)
 #define dpa__u_p_bo_refcounted__v_bo_simple(X) dpa_u_rescope(const dpa_u_bo_simple_t, (X)->bo_simple)
 #define dpa__u_cp_bo_refcounted__v_bo_simple(X) dpa_u_rescope(const dpa_u_bo_simple_t, (X)->bo_simple)
-#define dpa__u_any_bo_refcounted__v_bo_simple(X)dpa_u_rescope(const dpa_u_bo_simple_t, *(const dpa_u_bo_simple_t*)(X))
+#define dpa__u_any_bo_refcounted__v_bo_simple(X)dpa_u_rescope(const dpa_u_bo_simple_t, dpa__u_simple_fix_type(*(const dpa_u_bo_simple_t*)(X)))
 
-#define dpa__u_bo_with_refcount__v_bo_simple(X) dpa_u_rescope(const dpa_u_bo_simple_t, *(const dpa_u_bo_simple_t*)(X))
+#define dpa__u_bo_with_refcount__v_bo_simple(X) dpa_u_rescope(const dpa_u_bo_simple_t, dpa__u_simple_fix_type(*(const dpa_u_bo_simple_t*)(X)))
