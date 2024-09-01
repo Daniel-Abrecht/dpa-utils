f90241ceb8f5f850322d965a6550b1db4ccf978b
Daniel Abrecht <public@danielabrecht.ch>
2024-09-01 20:56:49 +0000

    implement all v_bo_simple conversions

--- include/dpa/utils/_bo/conv/bo_simple/v_bo_simple.h
+++ include/dpa/utils/_bo/conv/bo_simple/v_bo_simple.h
@@ -1,6 +1,7 @@
 
 #define dpa_u_v_bo_simple(...) dpa_u_assert_selection(dpa_u_v_bo_simple_g((__VA_ARGS__)))
 #define dpa_u_v_bo_simple_g(X) dpa_u_generic((X), \
+    dpa__u_helper_v_g(bo, v_bo_simple, (X)), \
     dpa__u_helper_v_g(bo_inline, v_bo_simple, (X)), \
     dpa__u_helper_v_g(bo_simple, v_bo_simple, (X)), \
     dpa__u_helper_v_g(bo_hashed, v_bo_simple, (X)), \
@@ -14,7 +15,15 @@
   )
 #define dpa__u_p_bo_inline__v_bo_simple(X) dpa_u_rescope(const dpa_u_bo_simple_t, dpa__u_p_bo_inline__t_bo_simple_p((X)))
 #define dpa__u_cp_bo_inline__v_bo_simple(X) dpa_u_invalid_selection
-#define dpa__u_any_bo_inline__v_bo_simple(X) dpa_u_rescope(const dpa_u_bo_simple_t, dpa__u_p_bo_inline__t_bo_simple_p((dpa_u_bo_inline_t*)(X)))
+#define dpa__u_any_bo_inline__v_bo_simple(X) dpa_u_invalid_selection // opaque types are const, so dpa_u_inline data is const too.
+
+#define dpa__u_v_bo__v_bo_simple(X) dpa_u_generic((X).all.all, \
+    const char*: dpa_u_invalid_selection, \
+    char*: dpa_u_rescope(const dpa_u_bo_simple_t, dpa__u_p_bo__t_bo_simple_p((dpa_u_bo_t*)(X).all.all)) \
+  )
+#define dpa__u_p_bo__v_bo_simple(X) dpa_u_rescope(const dpa_u_bo_simple_t, dpa__u_p_bo__t_bo_simple_p((X)))
+#define dpa__u_cp_bo__v_bo_simple(X) dpa_u_invalid_selection
+#define dpa__u_any_bo__v_bo_simple(X) dpa_u_invalid_selection
 
 #define dpa__u_v_bo_simple__v_bo_simple(X)   (X)
 #define dpa__u_p_bo_simple__v_bo_simple(X)  *(X)
@@ -31,4 +40,5 @@
 #define dpa__u_cp_bo_refcounted__v_bo_simple(X) dpa_u_rescope(const dpa_u_bo_simple_t, (X)->bo_simple)
 #define dpa__u_any_bo_refcounted__v_bo_simple(X)dpa_u_rescope(const dpa_u_bo_simple_t, dpa__u_simple_fix_type(*(const dpa_u_bo_simple_t*)(X)))
 
+// Note: no conversion from dpa_u_bo_with_hash, because it's const and can be a BO_INLINE.
 #define dpa__u_bo_with_refcount__v_bo_simple(X) dpa_u_rescope(const dpa_u_bo_simple_t, dpa__u_simple_fix_type(*(const dpa_u_bo_simple_t*)(X)))
